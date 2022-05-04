7fdde1f783c77c922eab2f0e2d967e64f455c9cf
IcarusAir <nicholasbmair@gmail.com>
2022-05-04 16:49:52 -0400

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -7,6 +10,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
+  product: Product | undefined;
   constructor() { }
 
   ngOnInit(): void {
